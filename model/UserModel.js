aab23e07f1a8613820e1b958709bc92af0b2c459
Gabriel Rocha <gabriel.lbr@outlook.com>
2023-06-16 22:01:51 -0400

    STRUCTURE: adding pages to pages folder

---
+++ model/UserModel.js
@@ -0,0 +1,24 @@
+class UserModel {
+    pets = [
+        {
+            id: 0,
+            name: "",
+            species: "",
+            age: 0,
+            profilePictureURL: "",
+            tutorFirsName: "",
+            tutorlastName: "",
+            loginAccess: "",
+            email: "",
+            bio: "",
+            posts: [
+                {
+                    title: "",
+                    content: "",
+                    imageUrl: "",
+                },
+            ],
+        },
+    ];
+    constructor() {}
+}
